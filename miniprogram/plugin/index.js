8d87fafdd41ef8ad643dcf8aa50c69845196dd46
liuchuancnong <liuchuancong@touscm.com>
2020-04-27 18:59:45 +0800

    添加readme

--- miniprogram/plugin/index.js
+++
@@ -1,1 +0,0 @@
-require("./runtime"),require("./vendors"),module.exports=(wx.webpackJsonp=wx.webpackJsonp||[]).push([[3],{"5":function(e,o,n){"use strict";Object.defineProperty(o,"__esModule",{"value":!0}),o.answer=void 0,o.sayHello=function sayHello(){console.log("Hello plugin!")};(function _interopRequireDefault(e){return e&&e.__esModule?e:{"default":e}})(n(1));o.answer=42}},[[5,0,1]]]);
